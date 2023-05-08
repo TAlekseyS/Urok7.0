387efe4950ce1d7260fd485d1bdae39f1002dd8a
Aleksey_T <talekseys@mail.ru>
2023-05-08 15:37:43 +0300

    Sdelal

---
+++ src/main/java/StatsService.java
@@ -0,0 +1,81 @@
+//package ru.netology.Urok7;
+public class StatsService {
+
+    public int amountMoney(int[] months) {
+        int allMoney = 0;
+        for (int i = 0; i < months.length; i++) {
+            allMoney = allMoney + months[i];
+        }
+        return allMoney;
+    }
+
+
+    public int avarageAmountMoney(int[] months) {
+        int allMoney = 0;
+        int avarageMoney = 0;
+        int i = 0;
+        for (; i < months.length; i++) {
+            allMoney = allMoney + months[i];
+        }
+        avarageMoney = allMoney / i;
+        return avarageMoney;
+    }
+
+    public int numberMonthHighSale(int[] months) {
+        int maxMonth = 0;
+        //int avarageMoney = 0;
+        int i = 0;
+        for (; i < months.length; i++) {
+            if (months[i] >= months[maxMonth]) {
+                maxMonth = i;
+            }
+        }
+        return maxMonth;
+    }
+
+    public int numberMonthLoseSale(int[] months) {
+        int minMonth = 0;
+        //int avarageMoney = 0;
+        int i = 0;
+        for (; i < months.length; i++) {
+            if (months[i] <= months[minMonth]) {
+                minMonth = i;
+            }
+        }
+        return minMonth;
+    }
+
+    public int amountMonthBelowAvarageSale(int[] months) {
+        int allMoney = 0;
+        int minMonth = 0;
+        int avarageMoney = 0;
+        int i = 0;
+        for (; i < months.length; i++) {
+            allMoney = allMoney + months[i];
+        }
+        avarageMoney = allMoney / i;
+        for (i = 0; i < months.length; i++) {
+            if (months[i] < avarageMoney) {
+                minMonth++;
+            }
+        }
+        return minMonth;
+    }
+
+    public int amountMonthHighAvarageSale(int[] months) {
+        int allMoney = 0;
+        int maxMonth = 0;
+        int avarageMoney = 0;
+        int i = 0;
+        for (; i < months.length; i++) {
+            allMoney = allMoney + months[i];
+        }
+        avarageMoney = allMoney / i;
+        for (i = 0; i < months.length; i++) {
+            if (months[i] > avarageMoney) {
+                maxMonth++;
+            }
+        }
+        return maxMonth;
+    }
+}
---
+++ src/test/java/StatsServiceTest.java
@@ -0,0 +1,78 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class StatsServiceTest {
+
+    @Test
+    public void showAllMoney() {
+        StatsService service = new StatsService();
+
+        int[] months = { 8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18 };
+
+        int expectedMoney = 180;
+        int actualMoney = service.amountMoney(months);
+
+        Assertions.assertEquals(expectedMoney, actualMoney);
+    }
+
+
+    @Test
+    public void showAvarageMoney() {
+        StatsService service = new StatsService();
+
+        int[] months = { 8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18 };
+
+        int expectedMoney = 15;
+        int actualMoney = service.avarageAmountMoney(months);
+
+        Assertions.assertEquals(expectedMoney, actualMoney);
+    }
+
+    @Test
+    public void showNumberMonthHighSales() {
+        StatsService service = new StatsService();
+
+        int[] months = { 8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18 };
+
+        int expectedMoney = 7;
+        int actualMoney = service.numberMonthHighSale(months);
+
+        Assertions.assertEquals(expectedMoney, actualMoney);
+    }
+
+    @Test
+    public void showNumberMonthLoseSales() {
+        StatsService service = new StatsService();
+
+        int[] months = { 8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18 };
+
+        int expectedMoney = 8;
+        int actualMoney = service.numberMonthLoseSale(months);
+
+        Assertions.assertEquals(expectedMoney, actualMoney);
+    }
+
+    @Test
+    public void showamountMonthBelowAvarageSale() {
+        StatsService service = new StatsService();
+
+        int[] months = { 8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18 };
+
+        int expectedMoney = 5;
+        int actualMoney = service.amountMonthBelowAvarageSale(months);
+
+        Assertions.assertEquals(expectedMoney, actualMoney);
+    }
+
+    @Test
+    public void showamountMonthHighAvarageSale() {
+        StatsService service = new StatsService();
+
+        int[] months = { 8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18 };
+
+        int expectedMoney = 5;
+        int actualMoney = service.amountMonthHighAvarageSale(months);
+
+        Assertions.assertEquals(expectedMoney, actualMoney);
+    }
+}
